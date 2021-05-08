55e47e9014dc50e0c93ae2e6850ed1442c707ed6
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-05-08 15:37:23 -0300

    Creating the class ChessPosition

--- Program.cs
+++ Program.cs
@@ -11,13 +11,16 @@ namespace Chess
         {
             try
             {
-                Board board = new Board(8, 8);
+                ChessPosition pos = new ChessPosition('c', 7);
+                Console.WriteLine(pos);
+                Console.WriteLine(pos.ToPosition());
+                // Board board = new Board(8, 8);
 
-                board.InsertPiece(new Rook(board, Color.Black), new Position(0, 0));
-                board.InsertPiece(new Rook(board, Color.White), new Position(1, 3));
-                board.InsertPiece(new King(board, Color.Black), new Position(2, 4));
-                board.InsertPiece(new King(board, Color.Black), new Position(0, 2));
-                Screen.PrintBoard(board);
+                // board.InsertPiece(new Rook(board, Color.Black), new Position(0, 0));
+                // board.InsertPiece(new Rook(board, Color.White), new Position(1, 3));
+                // board.InsertPiece(new King(board, Color.Black), new Position(2, 4));
+                // board.InsertPiece(new King(board, Color.Black), new Position(0, 2));
+                // Screen.PrintBoard(board);
             }
             catch (BoardException e)
             {
